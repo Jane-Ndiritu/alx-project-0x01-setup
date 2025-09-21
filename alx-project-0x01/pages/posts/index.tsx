2b6979185ed994f03f7732c23746237e3f32a112
jane <jayndiritu36@gmail.com>
2025-09-21 13:15:30 +0300

    Extending Post Page

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,39 +1,44 @@
-import PostCard from "../../components/common/PostCard";
-import Header from "../../components/layout/Header";
-import { PostProps } from "../../interfaces";
+import PostCard from "@/components/common/PostCard";
+import PostModal from "@/components/common/PostModal";
+import Header from "@/components/layout/Header";
+import { PostData, PostProps } from "@/interfaces";
+import { useState } from "react";
 
-interface PostsPageProps {
-  posts: PostProps[];
-}
+const Posts: React.FC<PostProps[]> = ({ posts }) => {
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [post, setPost] = useState<PostData | null>(null);
 
-const PostsPage: React.FC<PostsPageProps> = ({ posts }) => {
+
+  const handleAddPost = (newPost: PostData) => {
+    setPost({ ...newPost, id: posts.length + 1 });
+  };
+
+
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col h-screen">
       <Header />
-      <main className="p-4 flex-grow">
-        <div className="max-w-7xl mx-auto">
-          <div className="flex justify-between items-center mb-8">
-            <h1 className="text-2xl font-semibold">Post Content</h1>
-            <button className="bg-blue-700 px-4 py-2 rounded-full text-white hover:bg-blue-800 transition-colors">
-              Add Post
-            </button>
-          </div>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {posts.map((post: PostProps) => (
-              <PostCard
-                key={post.id}
-                title={post.title}
-                body={post.body}
-                userId={post.userId}
-                id={post.id}
-              />
-            ))}
-          </div>
+      <main className="p-4">
+        <div className="flex justify-between">
+          <h1 className=" text-2xl font-semibold">Post Content</h1>
+          <button onClick={() => setModalOpen(true)}
+            className="bg-blue-700 px-4 py-2 rounded-full text-white">Add Post</button>
+        </div>
+        <div className="grid grid-cols-3 gap-2 ">
+          {
+            posts?.map(({ title, body, userId, id }: PostProps, key: number) => (
+              <PostCard title={title} body={body} userId={userId} id={id} key={key} />
+            ))
+          }
         </div>
       </main>
+
+      {isModalOpen && (
+        <PostModal onClose={() => setModalOpen(false)} onSubmit={handleAddPost} />
+      )}
     </div>
   )
 }
+
 
 export async function getStaticProps() {
   const response = await fetch("https://jsonplaceholder.typicode.com/posts")
@@ -41,9 +46,9 @@ export async function getStaticProps() {
 
   return {
     props: {
-      posts: posts.slice(0, 12)
+      posts
     }
   }
 }
 
-export default PostsPage;
+export default Posts;
